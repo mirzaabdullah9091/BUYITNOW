bdadc4521373254f224e5813cb720049818225a0
mirzaabdullah9091 <141639119+mirzaabdullah9091@users.noreply.github.com>
2024-06-24 18:23:32 +0500

    Update next.config.mjs

--- buyitnow/next.config.mjs
+++ buyitnow/next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        DB_URI:"mongodb+srv://abdullahmirza9091:HondaCivic1459@abdullah.4xyfsua.mongodb.net/BUYITNOW",
+        DB_URI:"",
         // HOST_URL:"https://buyitnow-nine.vercel.app",
         HOST_URL:"http://localhost:3000",
-        NEXTAUTH_SECRET:"hondacivicxisshaper",
+        NEXTAUTH_SECRET:"",
         CLOUD_NAME:"diaxgbf6t",
         CLOUDINARY_API_KEY:"486687464557189",
-        CLOUDINARY_API_SECRET:"0zO_Us1TWjAh_ZMas3N6HtSfxNY",
+        CLOUDINARY_API_SECRET:"",
         STRIPE_PUBLIC_KEY:"pk_test_51LgtEeEiFmmcfKq1a3ZyBz4tcRrgIWid1Ebc5IyKrGqnVZreregXzqna9PYUvsK7aqnuoOoGrmwSV6i3zvFqir6600GKMRgHLh",
-        STRIPE_PRIVATE_KEY:"sk_test_51LgtEeEiFmmcfKq16NngJioJHNTOLpOGL4HhQiTfWQjw4ixSwPN3vd18DIzqyTTTPSicUbk11yLa8lCGmLxTyEf700GcHJUns8",
-        STRIPE_WEBHOOK_SECRET:"whsec_aeed4a77ec103bd1266cc9507f350aa8b42cae2c38260e072973b53c67a27a2e"
+        STRIPE_PRIVATE_KEY:"",
+        STRIPE_WEBHOOK_SECRET:""
     },
     images: {
         domains: ["res.cloudinary.com"],
